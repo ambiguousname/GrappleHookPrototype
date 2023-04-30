3f1548119b5c0981c6cb9bb0bb884a85333c32a4
Tyler K <tyly04@gmail.com>
2023-04-30 15:36:29 -0700

    Movement for jumping

--- src/prefabs/Player.js
+++ src/prefabs/Player.js
@@ -2,28 +2,70 @@ import { Grapple } from "./Grapple.js";
 
 export class Player {
 	static gameplaySettings = {
-		density: 0.001,
+		body: {
+			density: 0.001,
+			groundFriction: 0.05,
+			staticFriction: 0.05,
+			airFriction: 0.01,
+		},
 
-		acceleration: 1.1,
-		groundFriction: 0.05,
-		staticFriction: 0.05,
-		airFriction: 0.01,
+		movement: {
+			acceleration: 1.1,
+			jumpAcceleration: 10,
+		}
 	}
 
 	constructor(scene, x, y) {
-		this.body = scene.matter.add.rectangle(x, y, 40, 40, {
-			density: Player.gameplaySettings.density,
-			friction: Player.gameplaySettings.groundFriction,
-			frictionStatic: Player.gameplaySettings.staticFriction,
-			frictionAir: Player.gameplaySettings.airFriction,
+		this.scene = scene;
+		this.vector = this.scene.matter.vector;
+
+		this.body = this.scene.matter.add.rectangle(x, y, 40, 40, {
+			density: Player.gameplaySettings.body.density,
+			friction: Player.gameplaySettings.body.groundFriction,
+			frictionStatic: Player.gameplaySettings.body.staticFriction,
+			frictionAir: Player.gameplaySettings.body.airFriction,
 		});
 
+		this.groundedBody = null;
+
 		// Freeze rotation (looks less weird, and makes friction more useful):
-		scene.matter.body.setInertia(this.body, Infinity);
+		this.scene.matter.body.setInertia(this.body, Infinity);
 
-		this.grapple = new Grapple(scene, this.body, new Phaser.Math.Vector2(10, 5));
-		this.scene = scene;
+		this.grapple = new Grapple(this.scene, this.body, new Phaser.Math.Vector2(10, 5));
 
+		this.constructInput();
+
+		this.scene.matter.world.on("collisionactive", this.updatePlayerGrounded, this);
+		this.scene.matter.world.on("collisionend", this.endPlayerGrounded, this);
+	}
+
+	updatePlayerGrounded(event, bodyA, bodyB) {
+		if (bodyA.id === this.body.id || bodyB.id === this.body.id) {
+			for (let i = 0; i < event.pairs.length; i++) {
+				let pair = event.pairs[i];
+				if (!pair.isActive) {
+					continue;
+				}
+				if (bodyA.id === pair.bodyA.id && bodyB.id === pair.bodyB.id) {
+					if (this.vector.dot(pair.collision.normal, this.vector.create(0, 1)) > 0.9) {
+						if (bodyA.id === this.body.id) {
+							this.groundedBody = bodyB.id;
+						} else {
+							this.groundedBody = bodyA.id;
+						}
+					}
+				}
+			}
+		}
+	}
+
+	endPlayerGrounded(event, bodyA, bodyB) {
+		if ((bodyA.id === this.body.id || bodyB.id === this.body.id) && (this.groundedBody !== null)) {
+			this.groundedBody = null;
+		}
+	}
+
+	constructInput() {
 		this.scene.input.on("pointerdown", (pointer) => {
 			if (pointer.primaryDown) {
 				if (this.grapple.hasFired()) {
@@ -36,7 +78,6 @@ export class Player {
 
 		this.movementKeys = this.scene.input.keyboard.addKeys("W,S,A,D");
 
-		this.vector = this.scene.matter.vector;
 
 		// Map keys to direction:
 		for (let key in this.movementKeys) {
@@ -69,7 +110,15 @@ export class Player {
 			}
 		}
 
-		let newVelocity = this.vector.add(this.body.velocity, this.vector.mult(intendedMove, Player.gameplaySettings.acceleration));
+		let newVelocity = this.vector.add(this.body.velocity, this.vector.mult(intendedMove, Player.gameplaySettings.movement.acceleration));
+
+		if (this.jump.isDown) {
+			// Are we on the ground or attached to a web?
+			if (this.grapple.isHooked() || this.groundedBody !== null) {
+				this.grapple.cancel();
+				newVelocity = this.vector.add(newVelocity, this.vector.create(0, -Player.gameplaySettings.movement.jumpAcceleration));	
+			}
+		}
 
 		this.scene.matter.body.setVelocity(this.body, newVelocity);
 		// #endregion

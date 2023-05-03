62edbd23d3b110ee8bcb7f4c4caf37b15734c3ea
Stephanie Ramirez <130512983+starrami00@users.noreply.github.com>
2023-05-02 23:18:47 -0700

    Adding Menu Scene

---
+++ src/scenes/Menu.js
@@ -0,0 +1,25 @@
+export class Menu extends Phaser.Scene {
+    constructor() {
+        super("menuScene");
+    }
+
+    preload() {
+        // load title screen
+        this.load.image('TitleScreen', './assets/Temp Title Screen.png');
+    }
+
+    create() {
+        // title config
+        let title = this.add.sprite(0,0,'TitleScreen');
+        title.setOrigin(0,0);
+        
+        this.space = this.scene.input.keyboard.addKey("SPACE");
+    }
+
+    update() {
+        if(this.space.isDown) {
+            this.scene.start("PlayScene");
+        }
+    }
+    
+}
